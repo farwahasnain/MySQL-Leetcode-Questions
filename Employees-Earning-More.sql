22e1ed3dcde9f165e533a98cbeb859bb328db5a9
Farwah Hasnain <farwah.hasnain@gmail.com>
2026-02-27 06:57:53 +0100

    Added Employees Earning More Than Their Managers.

---
+++ Employees-Earning-More.sql
@@ -0,0 +1,63 @@
+-- 181. Employees Earning More Than Their Managers
+-- https://leetcode.com/problems/employees-earning-more-than-their-managers/
+
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| salary      | int     |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+ 
+
+Write a solution to find the employees who earn more than their managers.
+Return the result table in any order.
+The result format is in the following example.
+
+Example 1:
+
+Input: 
+Employee table:
++----+-------+--------+-----------+
+| id | name  | salary | managerId |
++----+-------+--------+-----------+
+| 1  | Joe   | 70000  | 3         |
+| 2  | Henry | 80000  | 4         |
+| 3  | Sam   | 60000  | Null      |
+| 4  | Max   | 90000  | Null      |
++----+-------+--------+-----------+
+Output: 
++----------+
+| Employee |
++----------+
+| Joe      |
++----------+
+
+-- Schema:
+
+CREATE TABLE Employee (
+    id INT PRIMARY KEY,
+    name VARCHAR(100),
+    salary INT,
+    managerId INT,
+    FOREIGN KEY (managerId) REFERENCES Employee(id)
+);
+
+INSERT INTO Employee (id, name, salary, managerId) VALUES
+(1, 'Joe', 70000, 3),
+(2, 'Henry', 80000, 4),
+(3, 'Sam', 60000, NULL),
+(4, 'Max', 90000, NULL);
+
+-- Solution:
+
+SELECT e.name AS Employee
+FROM Employee e
+JOIN Employee m
+  ON e.managerId = m.id
+WHERE e.salary > m.salary;
